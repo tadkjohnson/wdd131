878165eab9cb8f1f5a3d42be4f752d53107779f6
tadkjohnson <tadkjohnson@gmail.com>
2025-07-18 11:58:26 -0500

    interersting

--- week03/scripts/place.js
+++ week03/scripts/place.js
@@ -2,10 +2,3 @@ document.getElementById('currentyear').textContent = new Date().getFullYear();
 
 document.getElementById(`LastModified`).textContent = `Last Modified: ${document.lastModified}`;
 
-// const mininav = document.querySelector('navigation');
-// const hambutton = document.querySelector('#menu');
-
-// hamButton.addEventListener('click', () => {
-// 	navigation.classList.toggle('show');
-// 	hamButton.classList.toggle('show');
-// });
