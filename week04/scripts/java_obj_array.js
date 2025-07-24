d5af79f9ffcbc5404baefee1633508cb609334dd
tadkjohnson <tadkjohnson@gmail.com>
2025-07-23 19:56:22 -0500

    123

---
+++ week04/scripts/java_obj_array.js
@@ -0,0 +1,16 @@
+let person = {
+    name: "John Doe",
+    age: 30, \
+    profession: "Software Engineer",
+    hobbies: ["reading", "building computers", "hiking"],
+    address: {
+        street: "123 Camino Rd",
+        city: "Santa Rosa",
+        country: "Canada"
+    },
+    isEmployed: true,
+    greet: function () {
+        console.log('Hello, my name is ${this.name}.'):
+    }
+};
+
