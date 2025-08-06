0b35ebb513943fde7d9358fe5a73cac5d32efd89
tadkjohnson <tadkjohnson@gmail.com>
2025-08-06 16:34:11 -0500

    1

---
+++ project/script/getdates.js
@@ -0,0 +1,18 @@
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+document.body.innerHTML += `<p>Last Modified: ${document.lastModified}</p>`;
+
+
+
+
+// const currentYear = new Date().getFullYear();
+
+
+
+
+// didn't work.  alert did but isn't what is needed
+// const full = document.querySelector("#full");
+// alert(document.lastModified);
+
+// const currentYear = new Date().getFullYear();
+
--- project/script/project.js
+++ project/script/project.js
@@ -1,5 +1,5 @@
-document.getElementById("clickable1").style = "color: #fc7753; text-decoration:none; cursor:target;";
-document.getElementById("clickable1").addEventListener("click", function () {
-  window.location.href = "https://people.html";
-});
+// document.getElementById("clickable1").style = "color: #fc7753; text-decoration:none; cursor:target;";
+// document.getElementById("clickable1").addEventListener("click", function () {
+//   window.location.href = "https://people.html";
+// });
 
