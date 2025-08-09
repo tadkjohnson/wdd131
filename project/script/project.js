8b4f8f8e6a812b2b3a9231fe242c8ab74f6a3775
tadkjohnson <tadkjohnson@gmail.com>
2025-08-08 21:14:36 -0500

    done but js

--- project/script/project.js
+++ project/script/project.js
@@ -3,3 +3,8 @@
 //   window.location.href = "https://people.html";
 // });
 
+// page 1 needs a submit button
+// all pages need lazy loading on imaging
+// page 3 places needs playing w/the submit button 
+// pictures move domiaion nad warhammer on 2nd to last line. and then move warhammer and and warhammer to same page removing a spledor 
+
