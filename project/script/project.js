72e25565953fba5dbe7d905d0a805539ae03c50a
tadkjohnson <tadkjohnson@gmail.com>
2025-08-09 10:26:51 -0500

    46589

--- project/script/project.js
+++ project/script/project.js
@@ -4,13 +4,22 @@
 // page 3 places needs playing w/the submit button
 // pictures move domiaion nad warhammer on 2nd to last line. and then move warhammer and and warhammer to same page removing a spledor
 
+const hamburger = document.querySelector('.hamburger');
+
+document.querySelector('.hamburger').addEventListener('click', function () {
+    this.classList.toggle('active');
+});
+
+
+
+
 
 
 
 
 // navigation stuff with hamburger
 const navigation = document.querySelector('.navigation');
-const hamburger = document.querySelector('#hamburger');
+
 
 hamburger.addEventListener('click', () => {
     navigation.classList.toggle('show');   //  nav visibility on/off
@@ -19,11 +28,19 @@ hamburger.addEventListener('click', () => {
 
 
 // get title line to do something 
-const title = document.getElementById('title');
+const submit = document.getElementById('submit');
 title.addEventListener('click', () => {
-    alert('Welcome to the Table Top Gaming Club!');
+    alert('Welcome to the Club! We will respond ASAP!');
 });
 
 
+
+
+// const hamburger = document.querySelector('.hamburger');
+// const navigation = document.querySelector('.navigation');
+
+// hamburger.addEventListener('click', () => {
+//   navigation.classList.toggle('active');
+// });
 
 
